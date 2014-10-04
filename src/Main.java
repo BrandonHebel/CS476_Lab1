f707c1dbea56d884e326431648eb41ddb0a4188a
BrandonHebel <hebefrolfing@gmail.com>
2014-10-04 15:39:03 -0500

    now prints number of attempts

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 /**
  * CS476 Lab1
- * Guessing Game - Now with unlimited guesses!
+ * Guessing Game - Now tracks number of guesses!
  * 
  * @author Brandon Hebel
  */
@@ -11,16 +11,17 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int number = (int)((Math.random()*10)+1);
         int guess;
-        
+        int attempts = 1;
         while(true){
             System.out.print("Please guess a number from 1 to 10: ");
             guess = input.nextInt();
             if(guess == number) {
-                System.out.println("You have guessed the correct number! It is " + number + "!");
+                System.out.println("You have guessed the correct number in " + attempts + " attempt(s)! It is " + number + "!");
                 break;
             }
             else
                 System.out.println("You have guessed incorrectly. The number is not " + guess +".");
+            attempts++;
         } 
     } 
 }
