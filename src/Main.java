f98c03dfef040112ab552da2117988fb7cac0e72
BrandonHebel <hebefrolfing@gmail.com>
2014-10-04 15:29:13 -0500

    Added unlimited guesses

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 /**
  * CS476 Lab1
- * Guessing Game
+ * Guessing Game - Now with unlimited guesses!
  * 
  * @author Brandon Hebel
  */
 public class Main {
 
     public static void main(String[] args) {
-        
         Scanner input = new Scanner(System.in);
         int number = (int)((Math.random()*10)+1);
         int guess;
         
-        System.out.print("Please guess a number from 1 to 10: ");
-        guess = input.nextInt();
-        if(guess == number)
-            System.out.println("You have guessed the correct number!");
-        else
-            System.out.println("You have guessed incorrectly. The number is " + number +".");
+        while(true){
+            System.out.print("Please guess a number from 1 to 10: ");
+            guess = input.nextInt();
+            if(guess == number) {
+                System.out.println("You have guessed the correct number! It is " + number + "!");
+                break;
+            }
+            else
+                System.out.println("You have guessed incorrectly. The number is not " + guess +".");
+        } 
     } 
 }
